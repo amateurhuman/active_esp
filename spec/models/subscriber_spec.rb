0317477c0a35670eafa2e477f678fb8a1ababab9
Brian Morton <bmorton@sdreader.com>
2012-02-08 01:37:03 -0800

    Add tests for setting a name by full name.

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -11,4 +11,17 @@ describe ActiveESP::Subscriber do
     subscriber.last_name.should eq(@attributes[:last_name])
     subscriber.email.should eq(@attributes[:email])
   end
+
+  it "should assign the last word of a given full name as the last name" do
+    subscriber = ActiveESP::Subscriber.new
+    subscriber.name = "Some Really Long Name Morton"
+    subscriber.last_name.should eq("Morton")
+  end
+
+  it "should assign the all words except the last of a given full name as the first name" do
+    subscriber = ActiveESP::Subscriber.new
+    subscriber.name = "Billie Joe Armstrong"
+    subscriber.first_name.should eq("Billie Joe")
+  end
+
 end
