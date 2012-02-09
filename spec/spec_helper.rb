bc9781efc219136f63bd2c0947421ea004f1ed0c
Brian Morton <bmorton@sdreader.com>
2012-02-08 23:11:58 -0800

    Load all rspec support files in spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require 'active_esp'
 require 'factory_girl'
 require './spec/factories'
 
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
+
 RSpec.configure do |config|
   # some (optional) config here
 end
